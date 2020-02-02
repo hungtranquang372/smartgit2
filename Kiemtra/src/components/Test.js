f6b11cc3c145a68a594f307ddeaf86c789db9370
hungtranquang372 <hungtranquang372@gmail.com>
2020-02-02 11:41:57 +0700

    truyen state bang consumer trong class component

--- Kiemtra/src/components/Test.js
+++ Kiemtra/src/components/Test.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import{Hung} from '../context'
-export default function Test() {
+// import React from 'react'
+// import{Hung} from '../context'
+// export default function Test() {
 
-    return (
-        <div>
-            <Hung>
-                {value=>{
-                    const {name,vi}=value
-                    return `${name}-${vi}`
-                }}
-            </Hung>
+//     return (
+//         <div>
+//             <Hung>
+//                 {value=>{
+//                     const {name,vi}=value
+//                     return `${name}-${vi}`
+//                 }}
+//             </Hung>
             
-        </div>
-    )
+//         </div>
+//     )
+// }
+import React, { Component } from 'react'
+import {Hung} from '../context'
+export default class Test extends Component {
+    render() {
+        return (
+            <div>
+                <Hung>
+                    {
+                        value=>{
+                            const {name,vi}=value
+                            return `${vi}-${name}`
+                        }
+                    }
+                </Hung>
+            </div>
+        )
+    }
 }
